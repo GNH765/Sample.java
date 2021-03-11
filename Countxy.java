650b19eac83c983114ee77007901f9419f849845
GNH765 <80455742+GNH765@users.noreply.github.com>
2021-03-12 02:47:08 +0900

    Create Countxy.java
    メイン

---
+++ Countxy.java
@@ -0,0 +1,11 @@
+mport java.util.Arrays;  
+import java.util.Scanner;
+class Jw{
+  public static void main(String[] args){
+    int Jwww[];
+
+
+    Jww j1=new Jww();
+    System.out.println("個数");
+    j1.Input();
+}
